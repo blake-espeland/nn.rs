ca8e8a5b7f9b3705af5b65cc0e4ac4bf480299f4
blake-espeland <blake.espeland@gmail.com>
2022-10-24 13:15:53 -0500

    data loader building

--- src/util/data.rs
+++ src/util/data.rs
@@ -1,79 +1,101 @@
-use image::{RgbImage};
 use image::io::Reader as ImageReader;
-
+use image::RgbImage;
 use rand::seq::SliceRandom;
 use std::fs;
+use std::path::Path;
 
 pub enum InputType {
     Image,
     // Video,
     // Text,
     // Num,
-    // 
+    //
 }
 
-
-pub enum DataLoadingError<'a>{
-    EmptyDir(&'a str),
-    UnexpSubdir()
+pub enum DataLoadingError {
+    EmptyDir,
+    UnexpSubdir,
 }
 
 pub struct Dataset {
-    data_dir: String,
+    pub data_dir: String,
     data_paths: Vec<String>,
 
-    input_type: InputType,
+    pub input_type: InputType,
 }
 
+fn is_img_path(p: &Path) -> bool{
+    match p.extension() {
+        Some(pp) => {
+            match pp.to_str() {
+                Some(ppp) => {
+                    match ppp {
+                        "jpg" | "JPG" | "png" => true,
+                        _ => false
+                    }
+                },
+                None => false
+            }
+        },
+        None => false 
+    }
+}
 
-impl Dataset{
-    fn new(data_dir: &str) -> Result<Dataset, DataLoadingError>{
-        let mut pv: Vec<String>;
+impl Dataset {
+    pub fn new(data_dir: &str) -> Result<Dataset, DataLoadingError> {
+        let mut pv: Vec<String> = Vec::new();
 
-        let dir = fs::read_dir(data_dir).expect(format!("Could not load path: {}", data_dir).as_str());
+        let dir =
+            fs::read_dir(data_dir).expect(format!("Could not load path: {}", data_dir).as_str());
 
-        for item in dir{
+        for item in dir {
             let pth = item.unwrap().path();
-            
-            if pth.is_file(){
+
+            if pth.is_file() {
                 pv.push(pth.to_str().unwrap().to_string());
             }
         }
 
-        if pv.is_empty(){
-            return Err(DataLoadingError::<'b>::EmptyDir(format!("{} is an empty directory", data_dir).as_str()))
+        if pv.is_empty() {
+            return Err(DataLoadingError::EmptyDir);
         }
 
         Ok(Dataset {
             data_dir: data_dir.to_string(),
             data_paths: pv,
 
-            input_type: InputType::Image
+            input_type: InputType::Image,
         })
     }
 
-    fn batch_paths(&self, batch_size: usize) -> Vec<String> {
-        self.data_paths.choose_multiple(&mut rand::thread_rng(), batch_size).map(|x| x.clone()).collect()
+    pub fn batch_paths(&self, batch_size: usize) -> Vec<String> {
+        self.data_paths
+            .choose_multiple(&mut rand::thread_rng(), batch_size)
+            .map(|x| x.clone())
+            .collect()
     }
-
-
 }
 
-
-
-
-struct DataLoader{
+struct DataLoader {
     dataset: Dataset,
 
     batch_size: usize,
-    data_buff: Vec<RgbImage>
+    data_buff: Vec<RgbImage>,
 }
 
 impl DataLoader {
-    fn get_batch(&self) -> Vec<RgbImage>{
+    fn new(ds: Dataset, bs: usize) -> DataLoader {
+        DataLoader {
+            dataset: ds,
+            batch_size: bs,
+            data_buff: Vec::new(),
+        }
+    }
+
+    fn get_batch(&self) -> Vec<RgbImage> {
         let paths = self.dataset.batch_paths(self.batch_size);
         let mut vi = Vec::<RgbImage>::new();
-        for path in paths{
+        for path in paths {
             let reader = ImageReader::open(path).unwrap();
             let img = reader.decode().unwrap();
             let rgb = img.into_rgb8();
--- src/util/dtypes.rs
+++ src/util/dtypes.rs
@@ -38,3 +38,6 @@ impl Default for DataShape {
     }
 }
 
+pub struct Image {
+    
+}
